4f467ef7bfc96e92b448c128c5a8ac3f582d43d1
DoseDjalmadaDunhaJunior <djalminhajr_cunha@hotmail.com>
2020-04-13 13:56:00 -0300

    declarei as variaveis do python

---
+++ Python/venv/Decantador.py
@@ -0,0 +1,12 @@
+EtOH = 0
+Reator = 0
+glicerina = 0
+capacidade = 500
+ocupado = 0 # vai ser usado para gastar a capacidade
+Lancado = 0
+saida_glicerina = 0 # tem que ser 2%
+saida_EtOH = 0 # tem que ser 8%
+saida_solucao = 0 # tem que ser 90%
+pega = 0 # se o pega for 0 não é possivel pegar nada daqui
+recebe = 0 # se o recebe for 0 não é possivel receber nada daqui
+
